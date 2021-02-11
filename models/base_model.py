7c31207c75022a09c8c724278f134eb22bffab7e
faspen <aspensonf@gmail.com>
2021-02-11 16:01:04 +0000

    updated base model and added new tests

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,23 @@ class BaseModel():
 
     """ Defines common elements for future classes """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Constructor for base class """
+        '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+	'''
+        if len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    if k == "created_at" or k == "updated_at":
+                        v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Print string representation of object """
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -19,18 +19,39 @@ class TestBaseModel(unittest.TestCase):
         """ Test doc """
         self.assertIsNotNone(BaseModel.__doc__)
 
-    def test_base(self):
-        """ Test base """
+    def test_id(self):
+        """ Test id """
         b1 = BaseModel()
         b2 = BaseModel()
         self.assertNotEqual(b1.id, b2.id)
+
+    def test_name(self):
+        """ Test naming """
+        b1 = BaseModel()
         b1.name = "Base"
         self.assertEqual(b1.name, "Base")
+
+    def test_type(self):
+        """ Test type """
+        b1 = BaseModel()
         self.assertTrue(isinstance(b1.created_at, datetime))
         self.assertTrue(isinstance(b1.updated_at, datetime))
         self.assertTrue(isinstance(b1.to_dict(), dict))
         self.assertTrue(hasattr(b1, 'id'))
+
+    def test_attr(self):
+        """ Test if attrs exist """
+        b1 = BaseModel()
         self.assertTrue(hasattr(b1, 'created_at'))
         self.assertTrue(hasattr(b1, 'updated_at'))
+        self.assertTrue(hasattr(b1, '__init__'))
         dic = b1.to_dict()
         self.assertTrue(hasattr(dic, '__class__'))
+
+    def test_save(self):
+        """ Test save """
+        b1 = BaseModel()
+        self.assertTrue(hasattr(b1, 'created_at'))
+        b1.save()
+        self.assertTrue(hasattr(b1, 'updated_at'))
+        self.assertNotEqual(b1.created_at, b1.updated_at)
