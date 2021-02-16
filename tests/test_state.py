294fdeb0675710ed379933b943c8df16c2edbf7f
faspen <aspensonf@gmail.com>
2021-02-16 18:47:31 +0000

    state tests

---
+++ tests/test_state.py
@@ -0,0 +1,29 @@
+#!/user/bin/python3
+from models.base_model import BaseModel
+from models.state import State
+import unittest
+
+
+class TestState(unittest.TestCase):
+
+    """ Test State class """
+
+    def test_exist(self):
+        """see if instances exits"""
+        self.assertTrue(hasattr(State, "name"))
+
+    def test_id(self):
+        """Test id"""
+        s1 = State()
+        s2 = State()
+        self.assertNotEqual(s1.id, s2.id)
+
+    def test_attr(self):
+        """Test naming"""
+        s1 = State()
+        s1.name = "Montana"
+        self.assertEqual(s1.name, "Montana")
+
+    def test_doc(self):
+        """ Test doc """
+        self.assertIsNotNone(State.__doc__)
