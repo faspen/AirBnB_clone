e01853456457101360b35542c1a950c8425c4c82
faspen <aspensonf@gmail.com>
2021-02-17 16:17:32 +0000

    rookie error lol

--- models/place.py
+++ models/place.py
@@ -4,14 +4,14 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
+    """ Place class """
 
-    """Place class"""
     city_id = ""
     user_id = ""
     name = ""
     description = ""
-    number_rooms = ""
-    number_bathrooms = ""
+    number_rooms = 0
+    number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
     latitude = 0.0
