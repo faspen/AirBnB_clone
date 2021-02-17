ed7f5c2b0d685b9b843e97d8da037c683b46e0ef
faspen <aspensonf@gmail.com>
2021-02-17 18:22:41 +0000

    test console

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Test AirBnB console """
+import unittest
+from console import HBNBCommand
+
+
+class testCommand(unittest.TestCase):
+
+    """ Test command line """
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
