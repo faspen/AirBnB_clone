b6f4efb20c858575104afde78d6e25a6c4380c3f
faspen <aspensonf@gmail.com>
2021-02-17 16:18:49 +0000

    change to strings

--- models/review.py
+++ models/review.py
@@ -6,6 +6,6 @@ from models.base_model import BaseModel
 class Review(BaseModel):
 
     """Review class"""
-    place_id = []
-    user_id = []
-    text = []
+    place_id = ""
+    user_id = ""
+    text = ""
