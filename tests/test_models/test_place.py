8f4ecafd8ce81d771db2f86fa40f08753e1edb67
faspen <aspensonf@gmail.com>
2021-02-17 15:47:09 +0000

    test rearrangement

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,29 @@
+#!/user/bin/python3
+from models.base_model import BaseModel
+from models.place import Place
+import unittest
+
+
+class TestPlace(unittest.TestCase):
+
+    """ Test Place class """
+
+    def test_exist(self):
+        """ See if instances exist """
+        self.assertTrue(hasattr(Place, "name"))
+
+    def test_id(self):
+        """ Test id """
+        p1 = Place()
+        p2 = Place()
+        self.assertNotEqual(p1.id, p2.id)
+
+    def test_attr(self):
+        """ Test naming """
+        p1 = Place()
+        p1.name = "Hotel"
+        self.assertEqual(p1.name, "Hotel")
+
+    def test_doc(self):
+        """ Test doc """
+        self.assertIsNotNone(Place.__doc__)
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,29 @@
+#!/user/bin/python3
+from models.base_model import BaseModel
+from models.review import Review
+import unittest
+
+
+class TestReview(unittest.TestCase):
+
+    """ Test Review class """
+
+    def test_exist(self):
+        """ See if instances exist """
+        self.assertTrue(hasattr(Review, "text"))
+
+    def test_id(self):
+        """ Test id """
+        r1 = Review()
+        r2 = Review()
+        self.assertNotEqual(r1.id, r2.id)
+
+    def test_attr(self):
+        """ Test naming """
+        r1 = Review()
+        r1.text = "Pizza"
+        self.assertEqual(r1.text, "Pizza")
+
+    def test_doc(self):
+        """ Test doc """
+        self.assertIsNotNone(Review.__doc__)
